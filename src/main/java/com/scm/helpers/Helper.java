0a2218b7534f0d429c8c14c994b64c3a2479a8f1
YASWANTHBHERI <bheriyaswanth@gmail.com>
2025-02-28 12:51:22 +0530

    Update Helper.java

--- src/main/java/com/scm/helpers/Helper.java
+++ src/main/java/com/scm/helpers/Helper.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import javax.annotation.PostConstruct;
 
 public class Helper {
 
